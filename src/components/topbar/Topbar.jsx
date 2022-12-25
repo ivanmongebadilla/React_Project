f2826c984e6c473742a9221189565a52c6c183f5
Ivan Monge <ivanmongebadilla@gmail.com>
2022-12-25 09:59:17 -0800

    Top bar 2

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,12 +1,29 @@
 import React from 'react';
 import './topbar.css';
+import { Notifications, Language, Settings } from '@mui/icons-material';
 
 const Topbar = () => {
   return (
     <div className='topbar'>
         <div className='topbarWrapper'>
-            <div className='topLeft'>Left</div>
-            <div className='topRight'>Right</div>
+            <div className='topLeft'>
+              <span className='logo'>Admin</span>
+            </div>
+            <div className='topRight'>
+              <div className='topbarIconContainer'>
+                <Notifications />
+                <span className='topIconBadge'>2</span>
+              </div>
+              <div className='topbarIconContainer'>
+                <Language />
+              </div>
+              <div className='topbarIconContainer'>
+                <Settings />
+              </div>
+              <img src="https://www.shutterstock.com/image-photo/head-shot-portrait-close-smiling-260nw-1714666150.jpg" 
+                   alt='' 
+                   className='topAvatar' />
+            </div>
         </div>
     </div>
   )
