e13d66c9be527a19f9b2f5ab0e7cb13f3b34e75a
Ivan Monge <ivanmongebadilla@gmail.com>
2022-12-30 13:04:11 -0800

    12/30/2022

--- src/components/widgetsLg/WidgetsLarge.jsx
+++ src/components/widgetsLg/WidgetsLarge.jsx
@@ -2,9 +2,58 @@ import React from 'react';
 import './widgetsLarge.css';
 
 const WidgetsLarge = () => {
+
+  const Button = ({type}) => {
+    return <button className={'widgetLgButton ' + type} >{type}</button>
+  }
+
   return (
     <div className='widgetLg'>
-        WidgetsLarge
+        <h3 className='widgetLgTitle'>Latest transacionts</h3>
+        <table className='widgetLgTable'>
+          <tr className='widgetLgTr'>
+            <th className='widgetLgTh'>Customer</th>
+            <th className='widgetLgTh'>Date</th>
+            <th className='widgetLgTh'>Amount</th>
+            <th className='widgetLgTh'>Status</th>
+          </tr>
+          <tr className='widgetLgTr'>
+            <td className='widgetLgUser'>
+              <img className='widgetLgImg' src="https://www.shutterstock.com/image-photo/head-shot-portrait-close-smiling-260nw-1714666150.jpg" alt="" />
+              <span className='widgetLgName'>Carol Badilla</span>
+            </td>
+            <td className='widgetLgDate'>12 Dic 2022</td>
+            <td className='widgetLgAmount'>$236.34</td>
+            <td className='widgetLgStatus'><Button type="Approved" /></td>
+          </tr>
+          <tr className='widgetLgTr'>
+            <td className='widgetLgUser'>
+              <img className='widgetLgImg' src="https://www.shutterstock.com/image-photo/head-shot-portrait-close-smiling-260nw-1714666150.jpg" alt="" />
+              <span className='widgetLgName'>Carol Badilla</span>
+            </td>
+            <td className='widgetLgDate'>12 Dic 2022</td>
+            <td className='widgetLgAmount'>$236.34</td>
+            <td className='widgetLgStatus'><Button type="Pending" /></td>
+          </tr>
+          <tr className='widgetLgTr'>
+            <td className='widgetLgUser'>
+              <img className='widgetLgImg' src="https://www.shutterstock.com/image-photo/head-shot-portrait-close-smiling-260nw-1714666150.jpg" alt="" />
+              <span className='widgetLgName'>Carol Badilla</span>
+            </td>
+            <td className='widgetLgDate'>12 Dic 2022</td>
+            <td className='widgetLgAmount'>$236.34</td>
+            <td className='widgetLgStatus'><Button type="Declined" /></td>
+          </tr>
+          <tr className='widgetLgTr'>
+            <td className='widgetLgUser'>
+              <img className='widgetLgImg' src="https://www.shutterstock.com/image-photo/head-shot-portrait-close-smiling-260nw-1714666150.jpg" alt="" />
+              <span className='widgetLgName'>Carol Badilla</span>
+            </td>
+            <td className='widgetLgDate'>12 Dic 2022</td>
+            <td className='widgetLgAmount'>$236.34</td>
+            <td className='widgetLgStatus'><Button type="Approved" /></td>
+          </tr>
+        </table>
     </div>
   )
 };
--- src/components/widgetsSm/WidgetsSmall.jsx
+++ src/components/widgetsSm/WidgetsSmall.jsx
@@ -1,10 +1,68 @@
 import React from 'react';
 import './widgetsSmall.css';
+import { Visibility } from '@mui/icons-material';
 
 const widgetsSmall = () => {
   return (
     <div className='widgetSm'>
-        widgetsSmall
+        <span className="widgetSmTitle" > New Join Members</span>
+        <ul className='widgetSmList' >
+          <li className="widgetSmListItem" >
+            <img src='https://www.shutterstock.com/image-photo/head-shot-portrait-close-smiling-260nw-1714666150.jpg' alt='' className='widgetSmImg' />
+            <div className='widgetSmUser' >
+              <span className='widgetSmUserName'>Oscar Badilla</span>
+              <span className='widgetSmUserTitle'>Software Engineer</span>
+            </div>
+            <button className='widgetSmButton'>
+              <Visibility />
+              Display
+            </button>
+          </li>
+          <li className="widgetSmListItem" >
+            <img src='https://www.shutterstock.com/image-photo/head-shot-portrait-close-smiling-260nw-1714666150.jpg' alt='' className='widgetSmImg' />
+            <div className='widgetSmUser' >
+              <span className='widgetSmUserName'>Maria Hernandez</span>
+              <span className='widgetSmUserTitle'>Software Engineer</span>
+            </div>
+            <button className='widgetSmButton'>
+              <Visibility />
+              Display
+            </button>
+          </li>
+          <li className="widgetSmListItem" >
+            <img src='https://www.shutterstock.com/image-photo/head-shot-portrait-close-smiling-260nw-1714666150.jpg' alt='' className='widgetSmImg' />
+            <div className='widgetSmUser' >
+              <span className='widgetSmUserName'>Ana Barraza</span>
+              <span className='widgetSmUserTitle'>Software Engineer</span>
+            </div>
+            <button className='widgetSmButton'>
+              <Visibility />
+              Display
+            </button>
+          </li>
+          <li className="widgetSmListItem" >
+            <img src='https://www.shutterstock.com/image-photo/head-shot-portrait-close-smiling-260nw-1714666150.jpg' alt='' className='widgetSmImg' />
+            <div className='widgetSmUser' >
+              <span className='widgetSmUserName'>Carlos Jimenez</span>
+              <span className='widgetSmUserTitle'>Software Engineer</span>
+            </div>
+            <button className='widgetSmButton'>
+              <Visibility />
+              Display
+            </button>
+          </li>
+          <li className="widgetSmListItem" >
+            <img src='https://www.shutterstock.com/image-photo/head-shot-portrait-close-smiling-260nw-1714666150.jpg' alt='' className='widgetSmImg' />
+            <div className='widgetSmUser' >
+              <span className='widgetSmUserName'>Ethan Suk</span>
+              <span className='widgetSmUserTitle'>Software Engineer</span>
+            </div>
+            <button className='widgetSmButton'>
+              <Visibility className='widgetSmIcon' />
+              Display
+            </button>
+          </li>
+        </ul>
     </div>
   )
 };
